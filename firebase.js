3baff29b237bee80fdaf6edadbafe346bf5e7420
whiteroseandco <whiteroseandco.333@gmail.com>
2026-02-14 23:18:03 +0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,12 +1,1 @@
-// Import Firebase SDK
-// KONFIGURASI FIREBASE SESUAI PROJECT KAMU!
-const firebaseConfig = {
-    apiKey: "AIzaSyC8XzXXXXXXXXXXXXXXXXXXXXXXXXXXX",
-    authDomain: "whiterose-co-XXXX.firebaseapp.com",
-    databaseURL: "https://whiterose-co-XXXX-default-rtdb.firebaseio.com",
-    projectId: "whiterose-co-XXXX",
-    storageBucket: "whiterose-co-XXXX.appspot.com",
-    messagingSenderId: "XXXXXXXXX",
-    appId: "1:XXXXXXXXX:web:XXXXXXXXXXXXXX"
-};
-firebase.initializeApp(firebaseConfig);
+

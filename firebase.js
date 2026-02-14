345cdb97161bdf97433dfc6bc90dc2c5f33e1210
whiteroseandco <whiteroseandco.333@gmail.com>
2026-02-14 23:11:54 +0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,21 +1,12 @@
 // Import Firebase SDK
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
-import { getDatabase, ref, get, update, set } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js";
-
-// KONFIGURASI FIREBASE KAMU (GANTI SAMA DATA DI FIREBASE CONSOLE)
+// KONFIGURASI FIREBASE SESUAI PROJECT KAMU!
 const firebaseConfig = {
-  apiKey: "ISI_DENGAN_API_KEY_KAMU",
-  authDomain: "dashboard-whiterose.firebaseapp.com",
-  databaseURL: "https://dashboard-whiterose-default-rtdb.firebaseio.com",
-  projectId: "dashboard-whiterose",
-  storageBucket: "dashboard-whiterose.appspot.com",
-  messagingSenderId: "ISI_DENGAN_SENDER_ID_KAMU",
-  appId: "ISI_DENGAN_APP_ID_KAMU"
+    apiKey: "AIzaSyC8XzXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+    authDomain: "whiterose-co-XXXX.firebaseapp.com",
+    databaseURL: "https://whiterose-co-XXXX-default-rtdb.firebaseio.com",
+    projectId: "whiterose-co-XXXX",
+    storageBucket: "whiterose-co-XXXX.appspot.com",
+    messagingSenderId: "XXXXXXXXX",
+    appId: "1:XXXXXXXXX:web:XXXXXXXXXXXXXX"
 };
-
-// Inisialisasi Firebase
-const app = initializeApp(firebaseConfig);
-const db = getDatabase(app);
-
-// Export fungsi agar bisa dipakai di file lain
-export { db, ref, get, update, set };
+firebase.initializeApp(firebaseConfig);
